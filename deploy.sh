0dfc8926f1b13b5bcde2f32e7c61eec79db26f88
suveng <suveng@163.com>
2019-01-02 09:44:48 +0800

    auto deploy 2019-01-02 09:44:48

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-git add . &&git commit -m  "auto deploy %date% %time%" &&git push origin master
+time=$(date "+%Y-%m-%d %H:%M:%S");
+git add . &&git commit -m  "auto deploy ${time}" &&git push origin master
