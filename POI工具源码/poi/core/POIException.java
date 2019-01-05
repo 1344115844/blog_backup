db2c15d12e10bf771014095a047295d19609a7a6
suveng <suveng@163.com>
2019-01-05 20:25:48 +0800

    auto deploy 2019-01-05 20:25:47

---
+++ POI工具源码/poi/core/POIException.java
@@ -0,0 +1,19 @@
+
+package com.qw.gs.console.common.utils.poi.core;
+
+
+import com.qw.gs.console.common.exception.NestedRuntimeException;
+
+/**
+ * @description POI异常类
+ * @author <a href="linguozhi@52tt.com">soothing</a>
+ * @since 2016-07-13
+ * @version 1.0.0
+ */
+public class POIException extends NestedRuntimeException {
+
+	public POIException(String msg) {
+		super(msg);
+	}
+
+}
