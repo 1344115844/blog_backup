689c873a19841092d1706e8a44943faf4d2eaa93
suveng <suveng@163.com>
2019-02-26 11:47:21 +0800

    deploy at 2019-02-26 11:47:21

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+time=$(date "+%Y-%m-%d %H:%M:%S")
+git config core.autocrlf false
+git pull origin master 
+git add . -A
+git commit -m "deploy at ${time}"
+git push origin source 
+git push --force github master
